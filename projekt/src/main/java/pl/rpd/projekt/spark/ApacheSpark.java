d16ae1bf5aa45d81089aeffff6f2d34e1e540485
Daniel_Nowosielecki <danielnowo@gmail.com>
2021-11-26 15:35:40 +0100

    4: Prepare Apache Spark to proceed Cassandra data. FIX DEPENDENCIES (Those old were shitty.)

--- projekt/src/main/java/pl/rpd/projekt/spark/ApacheSpark.java
+++ projekt/src/main/java/pl/rpd/projekt/spark/ApacheSpark.java
@@ -1,6 +1,11 @@
 package pl.rpd.projekt.spark;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -37,7 +42,34 @@ public class ApacheSpark {
                 .option("table", "cryptocurrencies")
                 .load();
 
-        empDeptDataSet.show(1);
-        System.out.println("xD");
+        empDeptDataSet.select("volume").show(100);
+
+// Example jakies cos nie wiem jeszcze po co
+
+//        JavaRDD<String> idWithNameConcatenated = lines.flatMap(line -> {
+//            String[] data = COMMA.split(line);
+//            List<String> results = new ArrayList<>();
+//
+//            String id = data[1];
+//            String name = data[2];
+//            results.add(id + " " + name);
+//            return results;
+//        });
+//
+//        JavaRDD<Integer> amount = lines.flatMap(line -> {
+//            String[] data = COMMA.split(line);
+//            List<Integer> results = new ArrayList<>();
+//
+//            int returnAmount = Integer.parseInt(data[2]);
+//            results.add(returnAmount);
+//            return results;
+//        });
+//        JavaPairRDD<String, Integer> idNameWithAmount = idWithNameConcatenated.zip(amount);
+//        idWithNameConcatenated.collect();
+//        JavaPairRDD<String, Integer> counts = idNameWithAmount.reduceByKey(Integer::sum);
+//        JavaPairRDD<String, Integer> sorted = counts.sortByKey();
+//
+//        sorted.coalesce(1).saveAsHadoopFile(outputPath, Text.class, IntWritable.class, SequenceFileOutputFormat.class);
+//        sc.stop();
     }
 }
